a035880aef1c011407f080fbd295b7ffcf53d196
Bradiowave <divebombwalker@gmail.com>
2018-10-27 14:43:46 -0700

    create Hobbies component
    Hobbies component renders a list of Hobby components

--- auto_scheduler/src/components/Hobbies/Hobbies.js
+++ auto_scheduler/src/components/Hobbies/Hobbies.js
@@ -1,3 +1,16 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import './Hobbies.css';
+
+import Hobby from '../Hobby/Hobby.js';
+
+const Hobbies = (props) => {
+    return (
+        <div className="hobbyCardsCollection">
+            {this.props.hobbies.map(hobby => (
+                <Hobby hobby={hobby} />
+            ))}
+        </div>
+    )
+}
+
+export default Hobbies
