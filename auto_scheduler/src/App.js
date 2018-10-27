710cc1c9257b4c3b0281640c9e6028d47dd76dc6
Bradiowave <divebombwalker@gmail.com>
2018-10-27 14:32:04 -0700

    set up App.js
    initialize state, fill it with an axios call to the server, render the Hobbies component

--- auto_scheduler/src/App.js
+++ auto_scheduler/src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+
 import './App.css';
 
+import Hobbies from './components/Hobbies/Hobbies.js';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hobbies: [],
+      activeHobbies: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get('http://localhost:4088/api/hobbies')
+      .then(hobbies => {this.setState({hobbies: hobbies})})
+      .catch(err => {console.log(err)})
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <div className='hobbies'>
+          
+          <Route path='/hobbies' render={ (props) => {
+            return (<Hobbies {...props} activeHobbies={this.state.activeHobbies} hobbies={this.state.hobbies} />)
+          }}/>
+
+        </div>
       </div>
     );
   }
